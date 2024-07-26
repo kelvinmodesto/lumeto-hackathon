3a1d8b996397fee5b57246c7d50f5b27f683bc0a
Kelvin Modesto <km.modesto@gmail.com>
2024-07-26 16:02:24 -0300

    Add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/lumeto-hackathon',
   plugins: [react()],
   resolve: {
     alias: {
